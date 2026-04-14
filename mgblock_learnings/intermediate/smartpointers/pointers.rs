eae749ddb78aafa887608eeacebff48af55d9118
its-YogeshChandra <pandittheroyal@gmail.com>
2026-04-15 00:19:16 +0530

    update: added box smart pointers

--- mgblock_learnings/intermediate/smartpointers/pointers.rs
+++ mgblock_learnings/intermediate/smartpointers/pointers.rs
@@ -1,1 +1,17 @@
 //create smart pointers
+#[derive(Debug)]
+pub enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn main() {
+    use crate::List::{Cons, Nil};
+
+    //call the enum
+    let list = Cons(
+        1,
+        Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Nil))))))),
+    );
+    println!("the list is : {:#?}", list);
+}
